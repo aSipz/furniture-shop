03394ac75f484bd922cf785ccb5d680382aba43c
aSipz <115708388+aSipz@users.noreply.github.com>
2023-07-31 16:11:22 +0300

    Update index.js

--- server/config/index.js
+++ server/config/index.js
@@ -3,13 +3,13 @@ config = {
         PORT: 3000,
         SECRET: 'verySecretSecret',
         DB_URI: 'mongodb://127.0.0.1:27017/furni-shop',
-        origin: ['http://localhost:4200'],
+        origin: ['http://localhost:4200']
     },
     production: {
         PORT: 3000,
         SECRET: 'evenMoreSecretSecret',
         DB_URI: 'mongodb+srv://aSipz:WQGlcsJ79fXqmEMj@cluster0.yazycpi.mongodb.net/?retryWrites=true&w=majority',
-        origin: '*',
+        origin: ['http://localhost:4200', 'https://furniture-shop-df231.web.app']
     }
 };
 
