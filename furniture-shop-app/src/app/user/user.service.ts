72ab63e40a32a452ee957d76a895c1febb1cd819
aSipz <115708388+aSipz@users.noreply.github.com>
2023-07-31 14:12:49 +0300

    up

--- furniture-shop-app/src/app/user/user.service.ts
+++ furniture-shop-app/src/app/user/user.service.ts
@@ -52,7 +52,8 @@ export class UserService implements OnDestroy {
   }
 
   logout() {
-    this.cookieService.delete('auth');
+    // this.cookieService.delete('auth');
+    this.cookieService.set('auth', 'logout', Date.now(), undefined, undefined, true, 'None');
     return this.http.post<void>('/api/users/logout', {})
       .pipe(tap(() => this.user$$.next(null)));
   }
